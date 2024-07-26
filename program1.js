f0ca1524231d0c2d4c6f49eaeba39681aed12236
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-26 12:18:21 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -19,9 +19,9 @@ const getTotalIsles = function (grid) {
   // write your code here
   let output = 0;
 
-  for (int i = 0; i < grid.size(); i++)
+  for (i = 0; i < grid.size(); i++)
         {
-            for (int j = 0; j < grid[0].size(); j++)
+            for (let j = 0; j < grid[0].size(); j++)
             {
                 if (grid[i][j] == '1')
                 {
