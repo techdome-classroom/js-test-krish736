e3c85ad487b40d040bbbeb3aba7a17a00ecb366d
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-26 12:15:46 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,3 +1,18 @@
+void dfs(vector<vector<char>> &grid, int i, int j)
+    {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1')
+        {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+};
+
 const getTotalIsles = function (grid) {
 
 
