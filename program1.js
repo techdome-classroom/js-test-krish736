dab09cfb43234bb7f1e97c2105b511ca9bbda26a
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-26 12:18:22 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,38 +1,33 @@
-const dfs = (grid, i, j) =>
-    {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1')
-        {
-            return;
-        }
+const dfs = (grid, i, j) => {
+  if (
+    i < 0 ||
+    i >= grid.size() ||
+    j < 0 ||
+    j >= grid[0].size() ||
+    grid[i][j] != "1"
+  ) {
+    return;
+  }
 
-        grid[i][j] = '0';
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
-    }
-
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
+};
 
 const getTotalIsles = function (grid) {
-
-
   // write your code here
   let output = 0;
 
-  for (i = 0; i < grid.size(); i++)
-        {
-            for (let j = 0; j < grid[0].size(); j++)
-            {
-                if (grid[i][j] == '1')
-                {
-                    numIslands++;
-                    dfs(grid, i, j);
-                }
-            }
-        }
-
-
-
+  for (i = 0; i < grid.size(); i++) {
+    for (let j = 0; j < grid[0].size(); j++) {
+      if (grid[i][j] == "1") {
+        numIslands++;
+        dfs(grid, i, j);
+      }
+    }
+  }
 };
 
 module.exports = getTotalIsles;
