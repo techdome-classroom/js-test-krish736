86687651c546a2d8e15144c4528d7160c7219185
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-26 12:56:01 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,17 @@
 const decodeTheRing = function (s, p) {
 
     // write your code here
-    
+
+    let i = 0;
+    let j = 1;
+
+    while(i<s.length || j<p.length){
+      if(s[i] == p[j]){
+        i++;
+        j++;
+      }
+    }
+
   };
   
   module.exports = decodeTheRing;
