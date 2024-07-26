a809c020eda5702ab9089b2f86238ec0daf558a9
krish736 <112505529+krish736@users.noreply.github.com>
2024-07-26 13:11:03 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,44 +1,40 @@
 const decodeTheRing = function (s, p) {
+  // write your code here
 
-    // write your code here
+  let i = 0;
+  let j = 0;
 
-    let i = 0;
-    let j = 0;
+  while (i < s.length && j < p.length) {
+    if (s[i] == p[j]) {
+      i++;
+      j++;
+    }
 
-    while(i<s.length && j<p.length){
-      if(s[i] == p[j]){
-        i++;
-        j++;
-      }
+    if (p[j] == "?") {
+      i++;
+      j++;
+    }
 
-      if(p[j] == '?'){
-        i++;
-        j++;
-      }
-
-      if(p[j] == "*"){
-        let k = p[j+1];
-        while( i<s.length){
-          if(s[i] == k){
-            break;
-          }
-          i++;
+    if (p[j] == "*") {
+      let k = p[j + 1];
+      while (i < s.length) {
+        if (s[i] == k) {
+          break;
         }
         i++;
-        j++;
       }
-
-      console.log(s[i],p[j]);
+      i++;
+      j++;
     }
 
-    if(s[i] == p[j]){
-      return true;
-    }
-    else return false;
+    console.log(s[i], p[j]);
+  }
 
-  };
-  
-  module.exports = decodeTheRing;
+  if (s[i] == p[j]) {
+    return true;
+  } else return false;
+};
 
+module.exports = decodeTheRing;
 
-  decodeTheRing('hello', 'h*llo')
+decodeTheRing("hello", "h*llo");
